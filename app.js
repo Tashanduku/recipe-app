eeea111941b18b708308bf9d527ca15cb5e11081
Natasha Nduku <natashanduku0@gmail.com>
2024-12-18 13:29:11 +0300

    adds the dom elements

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const API_URL = 'https://www.themealdb.com/api/json/v1/1';
+
+    // DOM Elements
+    const searchForm = document.querySelector('.search');
+    const searchInput = document.querySelector('.search__field');
+    const resultsContainer = document.querySelector('.results');
+    const recipeContainer = document.querySelector('.recipe');
+    const bookmarksList = document.querySelector('.bookmarks__list');
+    const addRecipeBtn = document.querySelector('.nav__btn--add-recipe');
+    const bookmarksBtn = document.querySelector('.nav__btn--bookmarks');
+    const overlay = document.querySelector('.overlay');
+    const addRecipeWindow = document.querySelector('.add-recipe-window');
+    const ingredientContainer = document.querySelector('.ingredient-inputs');
+    const addIngredientBtn = document.querySelector('.btn--add-ingredient');
+})
