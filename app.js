d8fa1089e528be2e7c2a0b763fe9436bf9be6839
Natasha Nduku <natashanduku0@gmail.com>
2024-12-19 15:36:26 +0300

    if no nookmarks are there

--- app.js
+++ app.js
@@ -124,3 +124,25 @@ const toggleBookmark = async (recipeId) => {
         console.error('Error toggling bookmark:', error);
     }
 };
+
+// Render Bookmarks
+const renderBookmarks = () => {
+    if (state.bookmarks.length === 0) {
+        bookmarksList.innerHTML = `
+            <div class="message">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                    </svg>
+                </div>
+                <p>No bookmarks yet.</p>
+            </div>
+        `;
+        return;
+    }
+
+    bookmarksList.innerHTML = state.bookmarks
+        .map(generateRecipeMarkup)
+        .join('');
+};
+
